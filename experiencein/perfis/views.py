7fe3f1c8cc3d582349c4cd006a9edcfe600494c0
Nikolle de Lacerda Nascimento <67565460+nikollelacerda@users.noreply.github.com>
2021-11-10 20:52:18 -0300

    Add files via upload

---
+++ experiencein/perfis/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from perfis.models import Perfil
+
+def index(request):
+    return render(request, 'index.html')
+
+def exibir(request, perfil_id):
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', {"perfil" : perfil})
