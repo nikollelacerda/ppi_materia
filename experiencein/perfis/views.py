7a083ab776b748eade6ca06edfc94c0e0ff5f1c3
Nikolle de Lacerda Nascimento <67565460+nikollelacerda@users.noreply.github.com>
2021-12-21 22:49:19 +0000

    Modulo 8

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -5,11 +5,11 @@ from perfis.models import Perfil
 from django.shortcuts import redirect 
 
 def index(request):
-    return render(request, 'index.html', {'perfis' : Perfil.objects.all()})
+    return render(request, 'index.html', {'perfis' : Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request)})
 
 def exibir(request, perfil_id):
     perfil = Perfil.objects.get(id=perfil_id)
-    return render(request, 'perfil.html', {"perfil" : perfil})
+    return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request)})
 
 def convidar (request, perfil_id):
   perfil_a_convidar = Perfil.objects.get(id=perfil_id)
@@ -19,4 +19,7 @@ def convidar (request, perfil_id):
 
 def get_perfil_logado(request):
   return Perfil.objects.get(id=1)
+
+def aceitar(request, convite_id):
+  pass
 
