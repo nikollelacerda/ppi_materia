048a0353d0c0bf43e3f51d323221e3e9cb2b4ae3
Nikolle de Lacerda Nascimento <nikolledelacerda@gmail.com>
2022-01-08 10:25:46 -0300

    modulo 12.1

--- experiencein/experiencein/settings.py
+++ experiencein/experiencein/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL="/login/"
+LOGOUT_URL="/logout/"
+LOGIN_REDIRECT_URL="/"
+LOGOUT_REDIRECT_URL=LOGIN_URL
--- experiencein/usuarios/urls.py
+++ experiencein/usuarios/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from usuarios.views import RegistrarUsuarioView
 
 urlspatterns = [
-    path('registrar/', RegistrarUsuarioView.as_view(), name='registrar')
+    path('registrar/', RegistrarUsuarioView.as_view(), name='registrar'),
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout')
 ]
